ccdd0d134a5176574c2b3f4be2b877604e8411f9
Navit Choudhary <n.choudhary@deakin.edu.au>
2019-10-09 16:54:46 +1100

    added test branch with cloud credentials

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -5,8 +5,8 @@
 'use strict';
 
 var mongo = {
-  URI: process.env.MONGO_URI || 'mongodb://localhost/refugees',
-  //URI: process.env.MONGO_URI || "mongodb://"+process.env.MONGO_USER+":"+process.env.MONGO_PASS+"@localhost/"+process.env.MONGO_DBNAME_DEV,
+  //URI: process.env.MONGO_URI || 'mongodb://localhost/refugees',
+  URI: process.env.MONGO_URI || "mongodb://"+process.env.MONGO_USER+":"+process.env.MONGO_PASS+"@a064ca00-3e8f-4940-a0ca-18ca35c6fff9-0.22868e325a8b40b6840ed9895f3bb023.databases.appdomain.cloud:31051,a064ca00-3e8f-4940-a0ca-18ca35c6fff9-1.22868e325a8b40b6840ed9895f3bb023.databases.appdomain.cloud:31051/ibmclouddb?authSource=admin&replicaSet=replset",
   port: 27017
 };
 
--- utils/mongoConnect.js
+++ utils/mongoConnect.js
@@ -5,11 +5,15 @@
 'use strict';
 var Mongoose = require('mongoose');
 var Config = require('../config');
+var fs = require('fs');
 
+var path = require('path');
+var appDir = path.dirname(require.main.filename);
 
+var ca = [fs.readFileSync(`${appDir}/certs/certificate.crt`)];
 
 //Connect to MongoDB
-Mongoose.connect(Config.DBCONFIG.mongo.URI, { useNewUrlParser: true }, function (err) {
+Mongoose.connect(Config.DBCONFIG.mongo.URI, { useNewUrlParser: true,mongos: {ssl: true,sslValidate: false,sslCA:ca} }, function (err) {
   if (err) {
     console.log("DB Error: ", err);
     process.exit(1);
