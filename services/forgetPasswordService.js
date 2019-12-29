a54f4f842a0485f0046ec43d2531f557ead22cab
Andrea Mele <a.mele@deakin.edu.au>
2019-12-29 19:44:40 +1100

    fix reset psw

--- services/forgetPasswordService.js
+++ services/forgetPasswordService.js
@@ -4,15 +4,15 @@
 var Models = require('../models');
 
 var getForgetPasswordRequest = function (conditions, projection, options, callback) {
-  Models.forgetPassword.find(conditions, projection, options, callback);
+  Models.ForgetPassword.find(conditions, projection, options, callback);
 };
 var updateForgetPasswordRequest = function (criteria, dataToSet, options, callback) {
-  Models.forgetPassword.findOneAndUpdate(criteria, dataToSet, options, callback);
+  Models.ForgetPassword.findOneAndUpdate(criteria, dataToSet, options, callback);
 
 };
 
 var createForgetPasswordRequest = function (data, callback) {
-  var forgotPasswordEntry = new Models.forgetPassword(data);
+  var forgotPasswordEntry = new Models.ForgetPassword(data);
   forgotPasswordEntry.save(function (err, result) {
     callback(err, result);
   })
