87262e777f5b3e542862d088d433f170a326f7e8
Sanchit Dang <sanchitd5@live.com>
2020-08-21 12:25:24 +1000

    add image proxy for pdf generation

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 //External Dependencies
 require("dotenv");
 var Hapi = require("hapi");
+const https = require('https')
 
 //Internal Dependencies
 var Config = require("./config");
@@ -51,8 +52,44 @@ const init = async () => {
   server.route({
     method: "GET",
     path: "/",
-    handler: function(req, res) {
+    handler: function (req, res) {
       return res.view("welcome");
+    }
+  });
+
+  server.route({
+    method: "*",
+    path: "/helper/imageProxy",
+    handler: function (req, res) {
+      if (typeof req.query.url === 'undefined' && typeof req.query.responseType === 'undefined') {
+        return 'Cannot process this request';
+      }
+
+      if (req.query.responseType) {
+        let url = req.query.url;
+
+        return new Promise(function (resolve, reject) {
+          const options = new URL(url);
+
+          const reqHttps = https.request(options, (response) => {
+            console.log(`statusCode: ${response.statusCode}`)
+          })
+
+          reqHttps.on('response', function (d) {
+            resolve(res.response(d).type('image/png'));
+          });
+
+          reqHttps.on('error', (error) => {
+            console.error(error);
+            reject(error);
+          })
+
+          reqHttps.end()
+        });
+
+      } else {
+        return 'Cannot process this request.';
+      }
     }
   });
 
@@ -60,15 +97,15 @@ const init = async () => {
 
   SocketManager.connectSocket(server);
 
-  server.events.on("response", function(request) {
+  server.events.on("response", function (request) {
     console.log(
       request.info.remoteAddress +
-        ": " +
-        request.method.toUpperCase() +
-        " " +
-        request.url.pathname +
-        " --> " +
-        request.response.statusCode
+      ": " +
+      request.method.toUpperCase() +
+      " " +
+      request.url.pathname +
+      " --> " +
+      request.response.statusCode
     );
     console.log("Request payload:", request.payload);
   });
